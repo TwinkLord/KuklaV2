5e4862e803fcbfae07c8172ca2b1b7160691ca13
TwinkLord <mark.dovydovich@gmail.com>
2024-04-08 02:01:12 +0300

    Solved :)

--- piska/src/IntroPage/IntroPage.jsx
+++ piska/src/IntroPage/IntroPage.jsx
@@ -1,44 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import './IntroPage.css'; // Import CSS file
-import { Link } from 'react-router-dom'; // Import Link component from React Router
-import Homepage from '../HomePage/HomePage'; // Import the Homepage component
+import React, { useState, useEffect } from "react";
+import "./IntroPage.css"; // Import CSS file
+import Homepage from "../HomePage/HomePage"; // Import the Homepage component
 
 function IntroPage() {
-    const [clicked, setClicked] = useState(false);
-    const [displayText, setDisplayText] = useState('');
-    const [textDisplayed, setTextDisplayed] = useState(false);
-    const text = "Click me.";
+  const [clicked, setClicked] = useState(false);
+  const [displayIndex, setDisplayIndex] = useState(0);
+  const text = "Click me.";
 
-    useEffect(() => {
-        if (!textDisplayed) {
-            let index = 0;
-            const intervalId = setInterval(() => {
-                if (index === text.length) {
-                    clearInterval(intervalId);
-                    setTextDisplayed(true);
-                } else {
-                    setDisplayText(prevText => prevText + text[index]);
-                    index++;
-                }
-            }, 200);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (displayIndex < text.length) {
+        setDisplayIndex((prevIndex) => prevIndex + 1);
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 250);
 
-            return () => clearInterval(intervalId);
-        }
-    }, [text, textDisplayed]);
+    return () => clearInterval(intervalId);
+  }, [text, displayIndex]); // Include text and displayIndex in useEffect
 
-    const handleClick = () => {
-        setClicked(true);
-    };
+  const handleClick = () => {
+    setClicked(true);
+  };
 
-    return (
-        <div className={`container ${clicked ? 'slide-up' : ''}`}>
-            <div className="background"></div>
-            {/* Always render the "Click me." text */}
-            <div className="text" onClick={handleClick}>{textDisplayed ? text : displayText}</div>
-            {/* Conditionally render the Homepage component when clicked */}
-            {clicked && <Homepage />}
-        </div>
-    );
+  return (
+    <div className={`container ${clicked ? "slide-up" : ""}`}>
+      <div className="background"></div>
+      {/* Always render the "Click me." text */}
+      <div className="text" onClick={handleClick}>
+        {text.substring(0, displayIndex)}
+      </div>
+      {/* Conditionally render the Homepage component when clicked */}
+      {clicked && <Homepage />}
+    </div>
+  );
 }
 
 export default IntroPage;
